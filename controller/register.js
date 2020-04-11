6e82a9da0c8f6fe16241d81ad198f069c37a0a73
NakulGitHub <nkul.ic20@gmail.com>
2020-04-11 18:11:08 +0530

    checking error

--- controller/register.js
+++ controller/register.js
@@ -29,7 +29,10 @@ const handleRegister = (req, res, db, bcrypt) => {
 		.then(trx.commit)
 		.catch(trx.rollback)
 	})
-	.catch(err => res.status(400).json('unable to register'))
+	.catch(err => {
+		console.log("json response for reister::::",res.json());
+		res.status(400).json('unable to register')
+	})
 }
 
 module.exports =  {
