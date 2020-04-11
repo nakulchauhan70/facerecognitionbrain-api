76669cd8a4d27a9584da4f5621a8cd7d4ead572f
NakulGitHub <nkul.ic20@gmail.com>
2020-04-11 12:35:24 +0530

    reverting changes

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ const image = require('./controller/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		connectionString : process.env.DATABASE_URL,
-		ssl: true
+		host : '127.0.0.1',
+		user : 'postgres',
+		password : 'root',
+		database : 'smart-brain'
 	}
 });
 
@@ -34,4 +36,4 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT || 3000, () => { console.log('app is running on port ${process.env.PORT}'); })
+app.listen(process.env.PORT || 3000, ()=> { console.log('app is running on port ${process.env.PORT}'); })
